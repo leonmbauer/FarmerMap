789eb493ad1c64890308b8c62613114fefa16f7a
Leon Bauer <lbauer822@gmail.com>
2022-04-28 09:31:36 -0400

    Initial commit, added spring boot project

---
+++ src/test/java/com/leonbauer/FarmerMap/FarmerMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonbauer.FarmerMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmerMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
